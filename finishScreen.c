adf95f08f0df3545460fe26a74309c30c6ba0d5b
cbklvbinudx <cubekoll@gmail.com>
2023-03-16 08:19:37 +0100

    Finish screen visual update

--- finishScreen.c
+++ finishScreen.c
@@ -2,18 +2,23 @@
 #include "finishScreen.h"
 #include "raygui.h"
 #include "config.h"
+#include "beatmapInfo.h"
 
 int backButtonSwitch = 0;
 
 void DrawFinishScreen() {
     BeginDrawing();
 
-    ClearBackground(WHITE);
-    DrawText("Finished!", 100, 80, 48, GRAY);
+    DrawTexturePro(mapBackground, (Rectangle) { 0, 0, screenWidth, screenHeight },
+                   (Rectangle) { 0, 0, screenWidth, screenHeight }, (Vector2) { 0, 0 }, 0,
+                   WHITE);
+    DrawRectangleGradientV(0, 0, screenWidth, screenHeight, BLACK, BLANK);
 
-    DrawText(TextFormat("Great: %i", greatCounter), 100, 125, 20, GRAY);
-    DrawText(TextFormat("Good: %i", goodCounter), 100, 145, 20, GRAY);
-    DrawText(TextFormat("Misses: %i", missCounter), 100, 165, 20, GRAY);
+    DrawText("Finished!", 100, 55, 48, WHITE);
+
+    DrawText(TextFormat("Great: %i", greatCounter), 100, 100, 20, WHITE);
+    DrawText(TextFormat("Good: %i", goodCounter), 100, 120, 20, WHITE);
+    DrawText(TextFormat("Misses: %i", missCounter), 100, 140, 20, WHITE);
 
     backButtonSwitch = GuiButton((Rectangle){ 100, screenHeight - 200, screenWidth / 3, screenHeight / 7 },
                                            "Back");
